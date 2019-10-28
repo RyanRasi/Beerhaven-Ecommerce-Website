f107ce836d293ca6afdcea4b98af1239752f402b
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2019-10-28 15:17:18 +0000

    Add firebase config here

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,8 @@ import("firebase/storage");
 
   // Your web app's Firebase configuration
 
+  // 
+
   // Initialize Firebase
   const fb = firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
